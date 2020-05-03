eb61d0657b7968852aa7f1949ab22cd32d5a5712
Mohit <mkvmb.6@gmail.com>
2020-05-04 00:58:41 +0530

    Implemented Singleton Pattern using Lazy technique. AB#65

---
+++ DesignPatterns/DesignPatterns/Singleton/MySingleton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton
+{
+    public class MySingleton
+    {
+        private static readonly Lazy<MySingleton> myMySingleton = new Lazy<MySingleton>(() =>
+            new MySingleton());
+        private MySingleton()
+        {
+
+        }
+
+        public static MySingleton GetInstance()
+        {
+            return myMySingleton.Value;
+        }
+    }
+}
